209d982808ea4c652fa0a7f7ce8021fa34ebc168
pvlvukovic <44985904+pvlvukovic@users.noreply.github.com>
2019-12-22 11:34:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import mysql.connector
 
 app = Flask(__name__)
+
+konekcija = mysql.connector.connect(
+    passwd='root',
+    user='phpmyadmin',
+    database='evidencija_studenata',
+    port=3306
+)
+
+kursor = konekcija.cursor(dictionary=True)
 
 @app.route('/login')
 def login():
@@ -36,14 +46,56 @@ def predmet_izmena():
 
 @app.route('/korisnici')
 def korisnici():
-    return render_template('korisnici.html')
+    upit = "SELECT * FROM korisnici"
+    kursor.execute(upit)
+    korisnici = kursor.fetchall()
+    return render_template('korisnici.html', korisnici=korisnici)
 
-@app.route('/korisnik_novi')
+@app.route('/korisnik_novi', methods=['GET', 'POST'])
 def korisnik_novi():
-    return render_template('korisnik_novi.html')
+    if request.method == 'GET':
+        return render_template('korisnik_novi.html')
+    elif request.method == 'POST':
+        forma = request.form
+        upit = """ INSERT INTO 
+                    korisnici(ime,prezime,email,lozinka)
+                    VALUES (%s, %s, %s, %s)    
+                 """
+        vrednosti = (forma['ime'],forma['prezime'],forma['email'],forma['lozinka'])
+        kursor.execute(upit, vrednosti)
+        konekcija.commit()
+        return redirect(url_for('korisnici'))
 
-@app.route('/korisnik_izmena')
-def korisnik_izmena():
-    return render_template('korisnik_izmena.html')
+@app.route('/korisnik_izmena/<id>', methods=['GET', 'POST'])
+def korisnik_izmena(id):
+    if request.method == 'GET':
+        upit = "SELECT * FROM korisnici WHERE id=%s"
+        vrednost = (id,)
+        kursor.execute(upit, vrednost)
+        korisnik = kursor.fetchone()
+        return render_template('korisnik_izmena.html', korisnik=korisnik)
+    elif request.method == 'POST':
+        upit = """UPDATE korisnici SET 
+                    ime=%s,
+                    prezime=%s,
+                    email=%s,
+                    lozinka=%s
+                    WHERE id=%s    
+                """
+        forma = request.form
+        vrednosti = (forma['ime'],forma['prezime'],forma['email'],forma['lozinka'], id)
+        kursor.execute(upit, vrednosti)
+        konekcija.commit()
+        return redirect(url_for('korisnici'))       
+
+@app.route('/korisnik_brisanje/<id>', methods=['POST'])
+def korisnik_brisanje(id):
+    upit = """
+        DELETE FROM korisnici WHERE id=%s
+    """
+    vrednost = (id,)
+    kursor.execute(upit, vrednost)
+    konekcija.commit()
+    return redirect(url_for('korisnici'))
 
 app.run(debug=True)
